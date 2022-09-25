1882179929087a71ccc208ccf8ac0c95f370bf48
DiegoGUrra <clicknamk@gmail.com>
2022-09-25 03:36:00 -0300

    added createManyPeople

--- myApp.js
+++ myApp.js
@@ -26,7 +26,12 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople,(err,data)=>{
+    if(err){
+      done(err,null);
+    }
+    done(null,data);
+  })
 };
 
 const findPeopleByName = (personName, done) => {

8c442a369a316540bd42a221d4dffe4de5ad8516
DiegoGUrra <clicknamk@gmail.com>
2022-09-25 03:47:34 -0300

    added find people

--- myApp.js
+++ myApp.js
@@ -35,7 +35,13 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name:personName}, (err,data)=>{
+    if (err){
+      done(err,null);
+    }
+    done(null,data);
+  })
+
 };
 
 const findOneByFood = (food, done) => {
